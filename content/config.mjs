bd629021c70438cdb40c58be22b8c7f7fb26294b
tangyuan233 <36312785+tangyuan233@users.noreply.github.com>
2024-04-23 16:05:06 +0800

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -2,4 +2,12 @@
 /**
  * @type {import("../config/siteConfig").UserConfig}
  */
-export default {};
+const config = {
+  title: "XTY's Blog",
+  author: "LTang",
+  logo: "/images/logo.svg",
+  // 链接到你想要显示的网页，这会显示在导航栏和页脚
+  navLinks: [{ href: "/about", name: "About" }],
+};
+
+export default config;
