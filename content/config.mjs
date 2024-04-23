144e7dada421ede343953efa7bc972f8f57e3764
tangyuan233 <36312785+tangyuan233@users.noreply.github.com>
2024-04-23 16:11:04 +0800

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -4,9 +4,8 @@
  */
 const config = {
   title: "XTY's Blog",
+  description: "Welcome to my digital garden!",
   author: "LTang",
-  // 链接到你想要显示的网页，这会显示在导航栏和页脚
-  navLinks: [{ href: "/about", name: "About" }],
 };
 
 export default config;
